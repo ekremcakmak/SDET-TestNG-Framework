5bdf9a132ff5e965ac6a84d6ab858e8790b023a9
ekremcakmak <ekremcakmak@gmail.com>
2021-08-24 03:37:17 -0400

    Updates:
    - Added TestNG test framework example using POM.
    
    - Tools class has been updated. Fixed access issues to test elements due to late loading of the page.

--- src/test/java/com/TestNG/Utilities/Tools.java
+++ src/test/java/com/TestNG/Utilities/Tools.java
@@ -1,6 +1,38 @@
 package com.TestNG.Utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+
 public class Tools {
+
+
+    public static String getScreenshot(String name) throws IOException {
+        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        TakesScreenshot ts = (TakesScreenshot) DriverSetup.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+
+        File finalDestination = new File(target);
+        FileUtils.copyFile(source, finalDestination);
+        return target;
+    }
+
 
 public static void sleep(int millisecond)
 
@@ -16,4 +48,96 @@ public static void sleep(int millisecond)
 
 }
 
+
+
+    public static void switchToWindow(String targetTitle) {
+        String origin = DriverSetup.getDriver().getWindowHandle();
+        for (String handle : DriverSetup.getDriver().getWindowHandles()) {
+            DriverSetup.getDriver().switchTo().window(handle);
+            if (DriverSetup.getDriver().getTitle().equals(targetTitle)) {
+                return;
+            }
+        }
+        DriverSetup.getDriver().switchTo().window(origin);
+    }
+
+    //========Hover Over=====//
+    public static void hover(WebElement element) {
+        Actions actions = new Actions(DriverSetup.getDriver());
+        actions.moveToElement(element).perform();
+    }
+
+    //==========Return a list of string given a list of Web Element====////
+    public static List<String> getElementsText(List<WebElement> list) {
+        List<String> elemTexts = new ArrayList<>();
+        for (WebElement el : list) {
+            if (!el.getText().isEmpty()) {
+                elemTexts.add(el.getText());
+            }
+        }
+        return elemTexts;
+    }
+
+    //========Returns the Text of the element given an element locator==//
+    public static List<String> getElementsText(By locator) {
+        List<WebElement> elems = DriverSetup.getDriver().findElements(locator);
+        List<String> elemTexts = new ArrayList<>();
+        for (WebElement el : elems) {
+            if (!el.getText().isEmpty()) {
+                elemTexts.add(el.getText());
+            }
+        }
+        return elemTexts;
+    }
+
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
+        WebDriverWait wait = new WebDriverWait(DriverSetup.getDriver(), timeToWaitInSec);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static WebElement waitForVisibility(By locator, int timeout) {
+        WebDriverWait wait = new WebDriverWait(DriverSetup.getDriver(), timeout);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static WebElement waitForClickablility(WebElement element, int timeout) {
+        WebDriverWait wait = new WebDriverWait(DriverSetup.getDriver(), timeout);
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static WebElement waitForClickablility(By locator, int timeout) {
+        WebDriverWait wait = new WebDriverWait(DriverSetup.getDriver(), timeout);
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        try {
+            System.out.println("Waiting for page to load...");
+            WebDriverWait wait = new WebDriverWait(DriverSetup.getDriver(), timeOutInSeconds);
+            wait.until(expectation);
+        } catch (Throwable error) {
+            System.out.println(
+                    "Timeout waiting for Page Load Request to complete after " + timeOutInSeconds + " seconds");
+        }
+    }
+
+    //======Fluent Wait====//
+    public static WebElement fluentWait(final WebElement webElement, int timeinsec) {
+        FluentWait<WebDriver> wait = new FluentWait<WebDriver>(DriverSetup.getDriver())
+                .withTimeout(timeinsec, TimeUnit.SECONDS).pollingEvery(timeinsec, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+        WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return webElement;
+            }
+        });
+        return element;
+    }
 }
+
+
